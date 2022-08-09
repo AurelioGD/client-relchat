ba86378992a33a4d219f44f6b07be28a60a28131
Aurelio Gonzalez <agonzalez@sisu.mx>
2022-08-08 22:39:53 -0500

    Agrega control de errores en signup

--- src/views/signup/index.js
+++ src/views/signup/index.js
@@ -34,18 +34,20 @@ const Signup = () => {
     if (!AreEqual(password, passwordAgain)) return
 
     if(!isEmail(email)) return
-
-    const { user, session, error } = await signUp(email, password)
     
-    if (user && !error) {
-      const response = await createUser({name, username, email})
-
-      if(!response.error){
-        navigate(ROUTES.LOGIN, { replace: true })
+    try {
+      const { user, session, error } = await signUp(email, password)
+    
+      if (user && !error) {
+        const response = await createUser({name, username, email})
+  
+        if(!response.error){
+          navigate(ROUTES.LOGIN, { replace: true })
+        }
       }
+    } catch (error) {
+        console.error("Error: ", error)
     }
-
-    
   }
 
   return (
